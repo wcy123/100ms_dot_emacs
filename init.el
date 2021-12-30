a6896352a1b7a73384e30eb593989d77d6e27900
Wang Chunye <chunywan@xilinx.com>
2021-12-30 11:53:43 +0800

    add geiser

--- init.el
+++ init.el
@@ -695,7 +695,10 @@
   (add-hook 'racer-mode-hook #'company-mode)
   (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
   (setq company-tooltip-align-annotations t))
-
+;;; ------------------ for mzscheme ----------------------------
+(use-package geiser-chez
+  :config
+  (setq geiser-chez-binary "chezscheme"))
 ;; --- gcmh
 (use-package gcmh
   :diminish gcmh-mode
